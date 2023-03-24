6884e00f5099fc7f97943b6ad4ef11db68600cb2
Alex_QA <al694000n@gmail.com>
2023-03-24 17:16:20 +0300

    Delite comments

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,29 +12,22 @@ class BasePage:
         self.driver.get(self.url)
 
     def element_is_visible(self, locator, timeout=5):
-        # жди 10 секунд пока локатор не будет представлен
         return Wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
     def elements_are_visible(self, locator, timeout=5):
-        # жди 10 секунд пока все элементы не будут представлен
         return Wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
     def element_is_present(self, locator, timeout=5):
-        # найти элемент и взять текст из дом дерева(не обязательно чтобы был виден)
         return Wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
     def element_are_present(self, locator, timeout=5):
-        # найти элементы и взять текст из дом дерева(не обязательно чтобы был виден)
         return Wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
 
     def element_is_not_visible(self, locator, timeout=5):
-        # использование элемента который не виден
         return Wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
 
     def element_is_clickable(self, locator, timeout=5):
-        # кликабельный
         return Wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
     def go_to_element(self, element):
-        # перемещать нас к нужному элементу
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
